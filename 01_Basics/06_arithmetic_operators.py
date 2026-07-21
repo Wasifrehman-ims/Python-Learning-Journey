fc94b80fb59a85e51d9f9ca1ca0a4646b731de46
Wasifrehman-ims <wasifrehman165@gmail.com>
2026-07-21 06:34:12 -0700

    Add arithmetic operators example

---
+++ 01_Basics/06_arithmetic_operators.py
@@ -0,0 +1,16 @@
+# Arithmetic operators
+# This program demonstrates arithmetic operators in Python.
+
+num1 = 10
+num2 = 3
+
+print("Number 1:",num1)
+print("Number 2:",num2)
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponent:", num1 ** num2)
