456ffde382bdf65e4040d687a1a75f1ffb4319c8
Wasifrehman-ims <wasifrehman165@gmail.com>
2026-07-21 06:05:07 -0700

    Add files via upload

---
+++ 01_Basics/03_data_types.py
@@ -0,0 +1,12 @@
+# Data Types
+# We will learn different Data types
+
+name = "Wasif Rehman"
+age = 22
+height = 5.11
+is_student = True
+
+# The type() function returns the data type of a variable.print(name , type (name))
+print(age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
