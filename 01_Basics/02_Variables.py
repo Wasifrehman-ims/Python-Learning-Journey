fbdd34e299a48a0d4ceca3d02e284f2a9da97264
Wasifrehman-ims <wasifrehman165@gmail.com>
2026-07-21 05:43:16 -0700

    Create 02_Variables.py

---
+++ 01_Basics/02_Variables.py
@@ -0,0 +1,15 @@
+# What are variables? 
+# Variables are used to store data in memory.
+# Example
+
+name = 'Wasif rehman'
+age = 22
+country = "Pakistan"
+is_student = True
+
+print('Your name :',name)
+print("Your age :",age)
+print("Your country :",country)
+print("Student ?",is_student)
+
+print (name , age, country , is_student) # will print in same line
