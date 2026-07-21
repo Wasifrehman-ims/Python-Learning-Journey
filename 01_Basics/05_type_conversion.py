daaa0b5c69f922a817174e2e15228269fe522126
Wasifrehman-ims <wasifrehman165@gmail.com>
2026-07-21 06:21:44 -0700

    Add type conversion example using int()

---
+++ 01_Basics/05_type_conversion.py
@@ -0,0 +1,10 @@
+# Type Conversion
+# This program demonstrates type conversion using int().
+
+age = input("Enter your age: ")
+print("\nBefore conversion : ")
+print(type(age))
+
+age = int(age)
+print("\nAfter conversion :")
+print(type(age))
