f7938c09557c6d661a92d62b6cd3e12dd4fcb961
Wasifrehman-ims <wasifrehman165@gmail.com>
2026-07-21 06:44:53 -0700

    Add comparison operators example
    This program demonstrates various comparison operators in Python, including equality, inequality, and relational comparisons.

---
+++ 01_Basics/07_comparison_operators.py
@@ -0,0 +1,15 @@
+# Comparison operators
+# This program demonstrates comparison operators in Python.
+
+num1 = 10
+num2 = 20
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+
+print("\nEqual To:", num1 == num2 )
+print("Not Equal To:", num1 != num2)
+print("Greater Than:", num1 > num2)
+print("Less Than:", num1 < num2)
+print("Greater Than or Equal To:", num1 >= num2)
+print("Less Than or Equal To:", num1 <= num2)
